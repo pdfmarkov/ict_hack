b26af9997e1d9b32972629e8850655f0bd9a11a4
Petr Markov <channelpitnik@gmail.com>
2021-05-08 12:21:09 +0300

    try to fix some bugs x2

---
+++ src/main/java/org/comrades/springtime/module/Role.java
@@ -0,0 +1,20 @@
+package org.comrades.springtime.module;
+
+public enum Role {
+
+    STUDENT("STUDENT"),
+    MENTOR("MENTOR"),
+    MODERATOR("MODERATOR");
+
+
+    private final String roleName;
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return roleName;
+    }
+
+}
