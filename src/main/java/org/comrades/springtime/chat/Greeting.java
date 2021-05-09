7d9e5956165e47122b2c6a2a20292e19edaebfab
Petr Markov <channelpitnik@gmail.com>
2021-05-09 14:28:11 +0300

    add chat

---
+++ src/main/java/org/comrades/springtime/chat/Greeting.java
@@ -0,0 +1,10 @@
+package org.comrades.springtime.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
---
+++ src/main/java/org/comrades/springtime/chat/HelloMessage.java
@@ -0,0 +1,8 @@
+package org.comrades.springtime.chat;
+
+import lombok.Data;
+
+@Data
+public class HelloMessage {
+    private String name;
+}
