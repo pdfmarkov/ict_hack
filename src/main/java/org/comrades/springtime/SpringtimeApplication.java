d8738aaaebe5395701f8443ad94716208ae20936
Petr Markov <channelpitnik@gmail.com>
2021-05-09 14:43:40 +0300

    add Юра v3

--- src/main/java/org/comrades/springtime/SpringtimeApplication.java
+++ src/main/java/org/comrades/springtime/SpringtimeApplication.java
@@ -20,7 +20,7 @@ public class SpringtimeApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080", "https://iuribabalin.github.io/login");
             }
         };
     }
